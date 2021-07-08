dfbd1c6edfca2d6fd29ff64c49834c16b267ddc1
ArminKleinert <armin.kleinert@t-online.de>
2021-07-08 11:54:28 +0200

    Tokenization and toString seem to work now.

--- source/app.d
+++ source/app.d
@@ -4,12 +4,18 @@ import lyra.types;
 import lyra.reader;
 
 void main() {
-    auto tokens = tokenize("(define (f s n) (cons s n)) (f \"abc\" -3)");
-    writeln(tokens);
-    LyraObj o = make_ast(tokens);
-    writeln(o.type);
-    writeln(cons_id);
-    writeln(o);
+  writeln(obj(15L));
+  writeln(LyraObj.makeFixnum(19));
+  writeln(list([obj(1L), obj(0.5), obj("abc")]));
+  writeln(vector([]));
+  writeln(vector([obj(1L)]));
+  writeln(vector([obj(15L), obj(16L), obj(17L)]));
 
-    writeln(list([obj(1), obj(0.5), obj("abc")]));
+  import std.conv : to;
+
+  auto test = make_ast(tokenize("1 -2 0.5 () #t #f \"abc\" sym1 (list)"));
+  while (!test.isNil) {
+    writeln(test.car.toString ~ " " ~ to!string(test.car.type));
+    test = test.cdr;
+  }
 }
--- source/types.d
+++ source/types.d
@@ -1,6 +1,7 @@
 module lyra.types;
 
 import std.conv : to;
+import std.stdio;
 
 alias fixnum = long;
 alias floating = double;
@@ -11,90 +12,90 @@ alias LyraString = string;
 alias LyraChar = char;
 
 union Val {
-    Symbol symbol_val;
-    LyraString string_val;
-    char char_val;
-    fixnum fixnum_val;
-    floating real_val;
-    Func func_val;
-    bool bool_val;
-    Vector vector_val;
+  Symbol symbol_val;
+  LyraString string_val;
+  char char_val;
+  fixnum fixnum_val;
+  floating real_val;
+  Func func_val;
+  bool bool_val;
+  Vector vector_val;
 }
 
 enum : uint {
-    nil_id,
-    symbol_id,
-    string_id,
-    char_id,
-    fixnum_id,
-    real_id,
-    cons_id,
-    func_id,
-    env_id,
-    bool_id,
-    vector_id
+  nil_id,
+  symbol_id,
+  string_id,
+  char_id,
+  fixnum_id,
+  real_id,
+  cons_id,
+  func_id,
+  env_id,
+  bool_id,
+  vector_id
 }
 
 class Env {
-    private Env parent;
-    private LyraObj[Symbol] inner;
+  private Env parent;
+  private LyraObj[Symbol] inner;
 
-    alias inner this;
+  alias inner this;
 
-    this(Env parent) {
-        this.parent = parent;
+  this(Env parent) {
+    this.parent = parent;
+  }
+
+  LyraObj find(LyraObj sym) {
+    if (sym.type != symbol_id)
+      return null;
+
+    auto v = inner.get(sym.symbol_val, null);
+    if (v is null && parent !is null) {
+      return parent.find(sym);
     }
+    return cast(LyraObj) v;
+  }
 
-    LyraObj find(LyraObj sym) {
-        if (sym.type != symbol_id)
-            return null;
-
-        auto v = inner.get(sym.symbol_val, null);
-        if (v is null && parent !is null) {
-            return parent.find(sym);
-        }
-        return cast(LyraObj) v;
-    }
-
-    void set(LyraObj sym, LyraObj val) {
-        Symbol key = sym.symbol_val;
-        this[key] = val;
-    }
+  void set(LyraObj sym, LyraObj val) {
+    Symbol key = sym.symbol_val;
+    this[key] = val;
+  }
 }
 
 interface ILyraObj {
-    uint type();
-    LyraObj objtype();
+  uint type();
+  LyraObj objtype();
 }
 
 class LyraObj : ILyraObj {
-    private static LyraObj ly_true = null;
-    private static LyraObj ly_false = null;
+  private static LyraObj ly_true = null;
+  private static LyraObj ly_false = null;
 
-    alias value this;
+  alias value this;
 
-    uint _type = 0;
-    Val value = {bool_val: false};
+  uint _type = 0;
+  Val value = {bool_val: false};
 
-    public override uint type() {
-        return _type;
+  public override uint type() {
+    return _type;
+  }
+
+  public override LyraObj objtype() {
+    return makeFixnum(_type);
+  }
+
+  public static LyraObj makeBoolean(bool e) {
+    if (ly_true is null) {
+      Val t = {bool_val: true};
+      ly_true = new LyraObj(t, bool_id);
+      Val f = {bool_val: false};
+      ly_false = new LyraObj(f, bool_id);
     }
+    return e ? ly_true : ly_false;
+  }
 
-    public override LyraObj objtype() {
-        return makeFixnum(_type);
-    }
-
-    public static LyraObj makeBoolean(bool e) {
-        if (ly_true is null) {
-            Val t = {bool_val: true};
-            ly_true = new LyraObj(t, bool_id);
-            Val f = {bool_val: false};
-            ly_false = new LyraObj(f, bool_id);
-        }
-        return e ? ly_true : ly_false;
-    }
-
-    /*
+  /*
     public static LyraObj makeEnv(Env parent, Cons pairs) {
         Env envmap = new Env(parent);
         while (pairs !is Cons.nil()) {
@@ -107,180 +108,190 @@ class LyraObj : ILyraObj {
     }
     */
 
-    public static LyraObj makeFixnum(fixnum e) {
-        Val v = {fixnum_val: e};
-        return new LyraObj(v, fixnum_id);
-    }
+  public static LyraObj makeFixnum(fixnum e) {
+    Val v = {fixnum_val: e};
+    return new LyraObj(v, fixnum_id);
+  }
 
-    public static LyraObj makeSymbol(Symbol e) {
-        Val v = {symbol_val: e};
-        return new LyraObj(v, symbol_id);
-    }
+  public static LyraObj makeSymbol(Symbol e) {
+    Val v = {symbol_val: e};
+    return new LyraObj(v, symbol_id);
+  }
 
-    public static LyraObj makeString(LyraString e) {
-        Val v = {string_val: e};
-        return new LyraObj(v, string_id);
-    }
+  public static LyraObj makeString(LyraString e) {
+    Val v = {string_val: e};
+    return new LyraObj(v, string_id);
+  }
 
-    public static LyraObj makeChar(char e) {
-        Val v = {char_val: e};
-        return new LyraObj(v, char_id);
-    }
+  public static LyraObj makeChar(char e) {
+    Val v = {char_val: e};
+    return new LyraObj(v, char_id);
+  }
 
-    public static LyraObj makeReal(floating e) {
-        Val v = {real_val: e};
-        return new LyraObj(v, real_id);
-    }
+  public static LyraObj makeReal(floating e) {
+    Val v = {real_val: e};
+    return new LyraObj(v, real_id);
+  }
 
-    public static LyraObj makeCons(LyraObj car, LyraObj cdr) {
-        return Cons.create(car, cdr);
-    }
+  public static LyraObj makeCons(LyraObj car, LyraObj cdr) {
+    return Cons.create(car, cdr);
+  }
 
-    public static LyraObj makeFunc(Func e) {
-        Val v = {func_val: e};
-        return new LyraObj(v, func_id);
-    }
+  public static LyraObj makeFunc(Func e) {
+    Val v = {func_val: e};
+    return new LyraObj(v, func_id);
+  }
 
-    public static LyraObj makeVector(Vector e) {
-        Val v = {vector_val: e};
-        return new LyraObj(v, vector_id);
-    }
+  public static LyraObj makeVector(Vector e) {
+    Val v = {vector_val: e};
+    return new LyraObj(v, vector_id);
+  }
 
-    public static LyraObj makeEmpty() {
-        return Cons.nil();
-    }
+  public static LyraObj makeEmpty() {
+    return Cons.nil();
+  }
 
-    private this(Val value, uint type) {
-        this.value = value;
-        this._type = type;
-    }
+  private this(Val value, uint type) {
+    this.value = value;
+    this._type = type;
+  }
 
-    private this() {
-    }
+  private this() {
+  }
 
-    override string toString() {
-        switch (type) {
-        case symbol_id:
-            return this.symbol_val;
-        case string_id:
-            return "\"" ~ this.string_val ~ "\"";
-        case char_id:
-            return to!string(this.char_val);
-        case fixnum_id:
-            return to!string(this.fixnum_val);
-        case real_id:
-            return to!string(this.real_val);
-        case bool_id:
-            return this.bool_val ? "#t" : "#f";
-        case nil_id:
-            return "()";
-            //case vector_val:
-            //return to!string(this.vector_val());
-        default:
-            return "<LyraObj type=" ~ to!string(type) ~ ">";
-        }
+  override string toString() {
+    switch (type) {
+    case symbol_id:
+      return this.symbol_val;
+    case string_id:
+      return "\"" ~ this.string_val ~ "\"";
+    case char_id:
+      return to!string(this.char_val);
+    case fixnum_id:
+      return to!string(this.fixnum_val);
+    case real_id:
+      return to!string(this.real_val);
+    case bool_id:
+      return this.bool_val ? "#t" : "#f";
+    case nil_id:
+      return "()";
+    case vector_id:
+      import std.stdio;
+
+      Vector v = this.vector_val;
+      if (v.length == 0)
+        return "[]";
+      string res = "[";
+      for (auto i = 0; i < v.length; i++) {
+        res ~= v[i].toString();
+        res ~= " ";
+      }
+      res = res[0 .. $ - 1];
+      res ~= "]";
+      return res;
+    default:
+      return "<LyraObj type=" ~ to!string(type) ~ ">";
     }
+  }
 }
 
 class Cons : LyraObj {
-    private static Cons theEmptyList = null;
+  private static Cons theEmptyList = null;
 
-    private LyraObj _car = null;
-    private LyraObj _cdr = null;
+  private LyraObj _car = null;
+  private LyraObj _cdr = null;
 
-    public static Cons create(LyraObj _car, LyraObj _cdr) {
-        auto c = new Cons(_car, _cdr);
-        return c;
+  public static Cons create(LyraObj _car, LyraObj _cdr) {
+    auto c = new Cons(_car, _cdr);
+    return c;
+  }
+
+  public static Cons nil() {
+    if (theEmptyList is null) {
+      theEmptyList = new Cons(null, null);
+    }
+    return theEmptyList;
+  }
+
+  public Cons next() {
+    if (is(typeof(cdr) == Cons))
+      return cast(Cons) getcdr;
+    return theEmptyList;
+  }
+
+  public LyraObj getcar() {
+    return _car;
+  }
+
+  public LyraObj getcdr() {
+    return _cdr;
+  }
+
+  private this(LyraObj _car, LyraObj _cdr) {
+    this._car = _car;
+    this._cdr = _cdr;
+  }
+
+  override uint type() {
+    return (this is theEmptyList) ? nil_id : cons_id;
+  }
+
+  override LyraObj objtype() {
+    return LyraObj.makeFixnum(cons_id);
+  }
+
+  override string toString() {
+    string listToStringHelper(Cons cons) {
+      if (cons.isNil) {
+        return "";
+      }
+      if (cons.cdr.isNil) {
+        return cons.car.toString();
+      }
+      if (cons.cdr.type == cons_id) {
+        return cons.car.toString() ~ " " ~ listToStringHelper(cons.cdr.cons_val);
+      }
+      return cons.car.toString() ~ " . " ~ cons.cdr.toString();
     }
 
-    public static Cons nil() {
-        if (theEmptyList is null) {
-            theEmptyList = new Cons(null, null);
-        }
-        return theEmptyList;
-    }
-
-    public Cons next() {
-        if (is(typeof(cdr) == Cons))
-            return cast(Cons) getcdr;
-        return theEmptyList;
-    }
-
-    public LyraObj getcar() {
-        return _car;
-    }
-
-    public LyraObj getcdr() {
-        return _cdr;
-    }
-
-    private this(LyraObj _car, LyraObj _cdr) {
-        this._car = _car;
-        this._cdr = _cdr;
-    }
-
-    override uint type() {
-        return (this is theEmptyList) ? nil_id : cons_id;
-    }
-
-    override LyraObj objtype() {
-        return LyraObj.makeFixnum(cons_id);
-    }
-
-    override string toString() {
-        string listToStringHelper(Cons cons) {
-            if (cons.isNil) {
-                return "";
-            }
-            if (cons.cdr.isNil) {
-                return cons.car.toString();
-            }
-            if (cons.cdr.type == cons_id) {
-                return cons.car.toString() ~ " " ~ listToStringHelper(cons.cdr.cons_val);
-            }
-            return cons.car.toString() ~ " . " ~ cons.cdr.toString();
-        }
-
-        return "(" ~ listToStringHelper(this) ~ ")";
-    }
+    return "(" ~ listToStringHelper(this) ~ ")";
+  }
 }
 
 Symbol symbol_val(LyraObj obj) {
-    return obj.symbol_val;
+  return obj.symbol_val;
 }
 
 LyraString string_val(LyraObj obj) {
-    return obj.string_val;
+  return obj.string_val;
 }
 
 char char_val(LyraObj obj) {
-    return obj.char_val;
+  return obj.char_val;
 }
 
 fixnum fixnum_val(LyraObj obj) {
-    return obj.fixnum_val;
+  return obj.fixnum_val;
 }
 
 floating real_val(LyraObj obj) {
-    return obj.real_val;
+  return obj.real_val;
 }
 
 Cons cons_val(LyraObj obj) {
-    import std.stdio;
-
-    return cast(Cons) obj;
+  return cast(Cons) obj;
 }
 
 Func func_val(LyraObj obj) {
-    return obj.func_val;
+  return obj.func_val;
 }
 
 bool bool_val(LyraObj obj) {
-    return obj.bool_val;
+  return obj.bool_val;
 }
 
 Vector vector_val(LyraObj obj) {
-    return obj.vector_val;
+  return obj.vector_val;
 }
 
 /*
@@ -290,62 +301,62 @@ Env env_val(LyraObj obj) {
 */
 
 LyraObj cons(LyraObj car, LyraObj cdr) {
-    return Cons.create(car, cdr);
+  return Cons.create(car, cdr);
 }
 
 LyraObj car(LyraObj obj) {
-    if (obj.type == cons_id)
-        return (cast(Cons) obj).getcar;
-    return null;
+  if (obj.type == cons_id)
+    return (cast(Cons) obj).getcar;
+  return null;
 }
 
 LyraObj cdr(LyraObj obj) {
-    if (obj.type == cons_id)
-        return (cast(Cons) obj).getcdr;
-    return null;
+  if (obj.type == cons_id)
+    return (cast(Cons) obj).getcdr;
+  return null;
 }
 
 bool isNil(LyraObj obj) {
-    return obj is Cons.nil();
+  return obj is Cons.nil();
 }
 
 LyraObj symbol(Symbol e) {
-    return LyraObj.makeSymbol(e);
-}
-
-LyraObj obj(bool e) {
-    return LyraObj.makeBoolean(e);
+  return LyraObj.makeSymbol(e);
 }
 
 LyraObj obj(fixnum e) {
-    return LyraObj.makeFixnum(e);
-}
-
-LyraObj obj(LyraString e) {
-    return LyraObj.makeString(e);
-}
-
-LyraObj obj(char e) {
-    return LyraObj.makeChar(e);
+  return LyraObj.makeFixnum(e);
 }
 
 LyraObj obj(floating e) {
-    return LyraObj.makeReal(e);
+  return LyraObj.makeReal(e);
+}
+
+LyraObj obj(bool e) {
+  return LyraObj.makeBoolean(e);
+}
+
+LyraObj obj(LyraString e) {
+  return LyraObj.makeString(e);
+}
+
+LyraObj obj(char e) {
+  return LyraObj.makeChar(e);
 }
 
 LyraObj list(Vector e) {
-    LyraObj result = nil();
-    while (e.length > 0) {
-        result = cons(e[$ - 1], result);
-        e = e[0 .. $ - 1];
-    }
-    return result;
+  LyraObj result = nil();
+  while (e.length > 0) {
+    result = cons(e[$ - 1], result);
+    e = e[0 .. $ - 1];
+  }
+  return result;
 }
 
 LyraObj vector(Vector e) {
-    return LyraObj.makeVector(e);
+  return LyraObj.makeVector(e);
 }
 
 Cons nil() {
-    return Cons.nil();
+  return Cons.nil();
 }
