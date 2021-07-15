320c94ce1cc0072dd7b9c5fb49b41c0ec507c4d1
ArminKleinert <armin.kleinert@t-online.de>
2021-07-15 12:01:14 +0200

    Formatting.

--- app.d
+++ app.d
@@ -9,15 +9,15 @@ import lyrafunction;
 void main(string[] args) {
     import std.conv : to;
     import std.file : readText;
-    
+
     initializeGlobalEnv(Env.globalEnv);
-    
+
     auto code = make_ast(tokenize(readText("core.lyra")));
-    writeln(evalKeepLast(code , Env.globalEnv));
-    foreach(fname;args[1..$]) {
-    writeln("NOT HERE");
-    code = make_ast(tokenize(readText(fname)));
-    evalKeepLast(code , Env.globalEnv);
+    writeln(evalKeepLast(code, Env.globalEnv));
+    foreach (fname; args[1 .. $]) {
+        writeln("NOT HERE");
+        code = make_ast(tokenize(readText(fname)));
+        evalKeepLast(code, Env.globalEnv);
     }
 
     /*
