69cde1e417f59b5ebeb89b50c4ce253cb169d3d9
ArminKleinert <armin.kleinert@t-online.de>
2021-08-03 16:12:31 +0200

    Created  type.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-time dmd -O app.d types.d eval.d function.d reader.d buildins.d
+cd "./d source"
+time dmd -O app.d types.d eval.d function.d reader.d buildins.d && mv ./app ../app
+cd ..
 #time dmd -debug app.d types.d eval.d function.d reader.d buildins.d
